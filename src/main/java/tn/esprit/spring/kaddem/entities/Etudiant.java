3af0934ef6e56892676f2ac618f3b6217b1ee4df
hassenkekli@yahoo.com <hassenkekli@yahoo.com>
2024-11-08 04:49:41 +0100

    fgjhbvgefgrht

--- src/main/java/tn/esprit/spring/kaddem/entities/Etudiant.java
+++ src/main/java/tn/esprit/spring/kaddem/entities/Etudiant.java
@@ -1,6 +1,5 @@
 package tn.esprit.spring.kaddem.entities;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,9 +7,7 @@ import java.io.Serializable;
 import java.util.Set;
 import javax.persistence.*;
 @Entity
-@Getter
-@Setter
-@AllArgsConstructor
+@Getter@Setter
 public class Etudiant implements Serializable{
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
@@ -26,11 +23,25 @@ public class Etudiant implements Serializable{
     @JsonIgnore
     private Departement departement;
 
-
     public Etudiant() {
-
+        // TODO Auto-generated constructor stub
+    }
+    public Etudiant(String nomE, String rename) {
+        this.nomE = nomE;
+        this.prenomE = rename;
+    }
+    public Etudiant(String nomE, String rename, Option op) {
+        super();
+        this.nomE = nomE;
+        this.prenomE = rename;
+        this.op = op;
+    }
+    public Etudiant(Integer idEtudiant, String nomE, String rename, Option op) {
+        super();
+        this.idEtudiant = idEtudiant;
+        this.nomE = nomE;
+        this.prenomE = rename;
+        this.op = op;
     }
 
-    public Etudiant(String nomE, String prenomE) {
-    }
 }
